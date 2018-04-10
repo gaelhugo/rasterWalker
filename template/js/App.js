53a6ce17bac9e33883c03b614dbab3f2cb2a2b4d
Gael Hugo <gael.hugo@onemore-studio.com>
2018-04-10 10:57:55 +0200

    Update with letter (slow)

--- template/js/App.js
+++ template/js/App.js
@@ -13,7 +13,8 @@ class App {
     this.canvas.height = DIMENSION.height;
     this.ctx = this.canvas.getContext('2d');
     document.body.appendChild(this.canvas);
-
+    this.text = 'Les etoiles de la pub ';
+    this.indexLettre = 0;
     this.loadImage('andy.jpg');
   }
 
@@ -25,6 +26,11 @@ class App {
   }
   onImageLoaded(e) {
     // document.body.appendChild(this.img);
+
+    this.draw();
+  }
+
+  draw() {
     this.ctx.drawImage(this.img, 0, 0);
     this.imageDatas =
         this.ctx.getImageData(0, 0, this.img.width, this.img.height);
@@ -39,18 +45,30 @@ class App {
         let blue = this.imageDatas.data[index + 2];
         let brightness = Math.round(red * 0.3 + green * 0.59 + blue * 0.11);
 
-        let radius = (brightness / 255) * 6;
+        let radius = (brightness / 255) * 6 + Math.random() * 2;
 
-        this.ctx.fillStyle = 'rgb(' + red + ',' + blue + ',' + blue + ')';
+        this.ctx.fillStyle = 'rgba(' + red + ',' + blue + ',' + blue + ',1)';
         // this.ctx.fillRect(x, y, 6, 6);
         if (brightness > 50) {
-          this.ctx.beginPath();
-          this.ctx.arc(x * 2, y * 2, radius, 0, Math.PI * 2, false);
-          this.ctx.fill();
-          this.ctx.closePath();
+          // this.ctx.beginPath();
+          // this.ctx.arc(x * 2, y * 2, radius, 0, Math.PI * 2, false);
+          // this.ctx.fill();
+          // this.ctx.closePath();
+
+          this.ctx.fillStyle = 'rgb(255,255,255)';
+          this.ctx.fillStyle = 'rgba(' + red + ',' + blue + ',' + blue + ',1)';
+          this.ctx.font = radius * 4 + 'px Arial';
+          this.ctx.fillText(this.text[this.indexLettre], x * 2, y * 2);
+
+          if (this.indexLettre >= this.text.length - 1) {
+            this.indexLettre = 0;
+          } else {
+            this.indexLettre++;
+          }
         }
       }
     }
+    requestAnimationFrame(this.draw.bind(this));
   }
 }
 
